bbef014f166ce71a7ba04bf968438b3040118a0f
Guilherme Aranega <guilhermee.aranega@gmail.com>
2024-06-25 20:57:17 -0300

    add authentication route

---
+++ src/routes/authenticate-user.ts
@@ -0,0 +1,53 @@
+import { prisma } from "../lib/prisma";
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { createToken } from "./utils/create-token";
+import { compareHashedData } from "./utils/compare-hashed-data";
+
+export async function authenticateUser(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/users/authenticate",
+    {
+      schema: {
+        body: z.object({
+          email: z.string().email(),
+          senha: z.string().min(8),
+          tipo: z.enum(["empresa", "pessoa_fisica"]),
+        }),
+        response: {
+          200: z.object({
+            message: z.string(),
+            token: z.string(),
+          }),
+        },
+      },
+    },
+    async (req, res) => {
+      const data = req.body;
+
+      const user = await prisma.usuarios.findUnique({
+        where: { email: data.email },
+      });
+
+      if (!user) {
+        throw new Error("Email não cadastrado");
+      }
+
+      if (!(await compareHashedData(user.senha, data.senha))) {
+        throw new Error("Senha inválida");
+      }
+
+      const token = await createToken({
+        userId: user.id,
+        tipo: data.tipo,
+        email: data.email,
+      });
+
+      return res.status(200).send({
+        message: "Autenticado com sucesso",
+        token,
+      });
+    }
+  );
+}
