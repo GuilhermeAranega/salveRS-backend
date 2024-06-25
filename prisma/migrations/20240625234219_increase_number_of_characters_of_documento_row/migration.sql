9a012f8e7641d25f49ec0d9ce995d66578e64c93
Guilherme Aranega <guilhermee.aranega@gmail.com>
2024-06-25 20:55:52 -0300

    increase number of characters for documento row

---
+++ prisma/migrations/20240625234219_increase_number_of_characters_of_documento_row/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "prestadores" ALTER COLUMN "documento" SET DATA TYPE VARCHAR(255);
+
+-- AlterTable
+ALTER TABLE "usuarios" ALTER COLUMN "documento" SET DATA TYPE VARCHAR(255);
