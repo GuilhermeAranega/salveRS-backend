b08dfd86634f616887f2c785bcc47d2839dcc38b
Guilherme Aranega <guilhermee.aranega@gmail.com>
2024-07-25 01:49:37 -0300

    feat: add error handlers

---
+++ src/error-handler.ts
@@ -0,0 +1,25 @@
+import { FastifyInstance } from "fastify";
+import { BadRequest } from "./routes/_errors/bad-request";
+import { Unauthorized } from "./routes/_errors/unauthorized";
+import { ZodError } from "zod";
+
+type FastifyErrorHandler = FastifyInstance["errorHandler"];
+
+export const errorHandler: FastifyErrorHandler = (error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      message: "Error during validation",
+      errors: error.flatten().fieldErrors,
+    });
+  }
+
+  if (error instanceof BadRequest) {
+    return reply.status(400).send({ message: error.message });
+  }
+
+  if (error instanceof Unauthorized) {
+    return reply.status(400).send({ message: error.message });
+  }
+
+  reply.status(500).send({ message: "Internal server error" });
+};
