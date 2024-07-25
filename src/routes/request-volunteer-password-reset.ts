7876c92f2e50a9ea1f7baf9cc5e993fd6d5322ab
Guilherme Aranega <guilhermee.aranega@gmail.com>
2024-07-25 01:00:00 -0300

    feat: add request volunteer password reset email route

---
+++ src/routes/request-volunteer-password-reset.ts
@@ -0,0 +1,71 @@
+import { prisma } from "../lib/prisma";
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { sendEmail } from "./utils/send-email";
+
+export async function requestVolunteerPasswordReset(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/volunteers/reset",
+    {
+      schema: {
+        body: z.object({
+          email: z.string().email(),
+        }),
+        response: {
+          200: z.object({
+            message: z.string(),
+          }),
+        },
+      },
+    },
+    async (req, res) => {
+      const { email } = req.body;
+
+      const volunteer = await prisma.prestadores.findUnique({
+        where: { email },
+      });
+
+      if (!volunteer) {
+        throw new Error("Email não cadastrado");
+      }
+
+      const token = await res.jwtSign(
+        {
+          email: email,
+          userId: volunteer.id,
+          pwdRestore: true,
+        },
+        {
+          sign: { expiresIn: "10m", key: volunteer.id + volunteer.senha },
+        }
+      );
+
+      const existingToken = await prisma.links.findUnique({
+        where: { userId: volunteer.id },
+      });
+
+      if (existingToken) {
+        await prisma.links.delete({
+          where: { userId: volunteer.id },
+        });
+      }
+
+      await prisma.links.create({
+        data: {
+          userId: volunteer.id,
+          token,
+        },
+      });
+
+      /* const data = await sendEmail(email, token);
+      
+      if (!data) {
+        throw new Error("Não foi possível enviar o email");
+      } */
+
+      console.log(token);
+      return res.status(200).send({ message: "Email mandado com sucesso" });
+    }
+  );
+}
